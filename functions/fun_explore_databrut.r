678cf3bfe89f37287a3b04980496bf79ae8fbc5e
Romain Lorrilliere <romainlorrilliere@gmail.com>
2020-04-15 12:06:29 +0200

    Update fun_explore_databrut.r

--- functions/fun_explore_databrut.r
+++ functions/fun_explore_databrut.r
@@ -11,11 +11,11 @@ findDataInVP <- function(d=NULL,nomFile="export_stoc_10012019.txt",id_carre=NULL
     }
 
   #  browser()
-    if(!is.null(id_carre)) d <- subset(d,N..CarrÃ©.EPS %in% paste("CARRE NÂ°",id_carre,sep=""))
-    if(!is.null(num_point))  d <- subset(d, EXPORT_STOC_TEXT_EPS_POINT %in% paste("Point NÂ°",sprintf("%02d",num_point),sep=""))
+    if(!is.null(id_carre)) d <- subset(d,N..Carré.EPS %in% paste("CARRE N°",id_carre,sep=""))
+    if(!is.null(num_point))  d <- subset(d, EXPORT_STOC_TEXT_EPS_POINT %in% paste("Point N°",sprintf("%02d",num_point),sep=""))
     if(!is.null(date))  d <- subset(data,Date== format(as.Date(date),"%d.%m.%Y"))
     if(!is.null(annee)) d <- subset(d,year(as.Date(Date,"%d.%m.%Y"))%in% annee)
-    if(!is.null(espece)) d <- subset(d,EspÃ¨ce %in% espece)
+    if(!is.null(espece)) d <- subset(d,Espèce %in% espece)
     if(!is.null(distance_contact)) d <- subset(d, Distance.de.contact %in% distance_contact)
 
     return(d)
